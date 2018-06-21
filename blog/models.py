2e7cea13adc2fd9c219da354cdad6ea40efc2a71
Your Name <t_j4331@naver.com>
2018-06-21 17:30:43 +0900

    집에서 오류찾아보기

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -17,3 +18,39 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comment')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
+
+class Reporter(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=100)
+    pub_date = models.DateField()
+    reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.headline
+
+    class Meta:
+        ordering = ('headline',)
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
+from mysite.views import index
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'', index, name='home'),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
+    url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
-    url(r'', include('blog.urls', namespace='blog'))
+    url(r'^admin/', admin.site.urls),
 ]
